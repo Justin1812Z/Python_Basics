9ec2b4c002af134970e530d56a6b9521c837352a
Justin Zitel <justin.l.zitel@gmail.com>
2022-08-24 15:06:10 -0400

    initial commit

---
+++ VariableAndTypes.py
@@ -0,0 +1,10 @@
+mystring = str("hello")
+myfloat = float(10.0)
+myint = int(20)
+
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
